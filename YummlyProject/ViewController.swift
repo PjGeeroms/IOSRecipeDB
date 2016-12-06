a1e67ce2fc730b714e17500a46960508575577f5
Pieter-Jan Geeroms <pieterjangeeroms@hotmail.com>
2016-12-06 13:05:55 +0100

    Initial Commit

---
+++ YummlyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YummlyProject
+//
+//  Created by Pieter-Jan Geeroms on 06/12/2016.
+//  Copyright © 2016 Pieter-Jan Geeroms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
